c986f841fcc36a9b39e73a675a05f50c713105d7
Ryan Buswell <viaakiaa@ryans-macbook-pro.local>
2018-10-19 02:11:46 +0800

    Initial Commit

---
+++ Emoji Dictionary/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Emoji Dictionary
+//
+//  Created by Ryan Buswell on 10/19/18.
+//  Copyright © 2018 Ryan Buswell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
